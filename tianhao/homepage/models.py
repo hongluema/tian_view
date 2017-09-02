d29209bbdab7899df83d362e354e82a83fd96748
麻宏略 <ksy-walry@mahonglvedeMacBook-Pro.local>
2017-09-02 10:03:41 +0800

    change model

--- tianhao/homepage/models.py
+++ tianhao/homepage/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+import MySQLdb
 # Create your models here.
+
+from MySQLdb.cursors import DictCursor
+
+
+class MysqlServer(object):
+
+    def __init__(self):
+        self.conn = MySQLdb.connect(host="5670ff801929d.sh.cdb.myqcloud.com", user='cdb_outerroot', passwd='Y02389KSYHZ',port=4099)
+        self.cursor = self.conn.cursor(DictCursor)
+
+    def choice_db(self,db):
+        sql = "use {}".format(db)
+        self.cursor.execute(sql)
+
+    def query(self,sql):
+        self.choice_db("youpao")
+        self.cursor.execute(sql)
+        all_row = self.cursor.fetchall()
+        return all_row
+
+def test():
+    my = MysqlServer()
+    sql = "select * from redenvelope"
+    all_row = my.query(sql)
+    print all_row[6]
+
+
