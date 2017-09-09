3eef06820de9d8e1d980779f01d3756dc422e4e5
麻宏略 <ksy-walry@mahonglvedeMacBook-Pro.local>
2017-09-09 18:06:45 +0800

    change

--- tianhao/restart.sh
+++ tianhao/restart.sh
@@ -1,5 +1,3 @@
-
 #!/bin/sh
-
-ps aux|grep tt.ini|grep -v grep|cut -c 9-15|xargs kill -s 9
-uwsgi --ini /home/mahl/tian_view/tianhao/tt.ini & /usr/local/nginx-1.5.6/sbin/nginx &
+ps aux|grep tianhao.ini|grep -v grep|cut -c 9-15|xargs kill -s 9
+setsid nohup uwsgi tianhao.ini &
