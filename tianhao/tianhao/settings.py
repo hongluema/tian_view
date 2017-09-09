a307d1d827e084734aeff1da4dcdf1c1e4c82f74
麻宏略 <ksy-walry@mahonglvedeMacBook-Pro.local>
2017-09-09 15:41:17 +0800

    change

--- tianhao/tianhao/settings.py
+++ tianhao/tianhao/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'tianhao.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tianlian',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'CHARSET': 'utf-8',
     }
 }
 
